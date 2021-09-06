b74d66e9b20e609fd00bb4d3aff64175319c67ed
Paul Fell <swellpf@gmail.com>
2021-09-06 16:43:14 -0400

    refactor tick method and added get and set methods

--- index.js
+++ index.js
@@ -24,8 +24,15 @@ onDurationChange(){
 }
 
 tick = () => {
-    const timeRemaining = parseFloat(this.durationInput.value);
-    this.durationInput.value = timeRemaining - 1;
+    this.timeRemaining = this.timeRemaining - 1;
+}
+
+get timeRemaining() {
+    return parseFloat(this.durationInput.value);
+}
+
+set timeRemaining(time) {
+    this.durationInput.value = time;
 }
 
 }
